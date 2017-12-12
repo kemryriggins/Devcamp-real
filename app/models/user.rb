dabf67a73804848a2c6f2bcf4c7f37f81f7c41f5
kemryriggins <98kariggins@gmail.com>
2017-12-12 12:38:44 -0700

    test commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-   
-   validates_presence_of :name
 
-   def first_name
-   	 self.name.split.first
-   end
+  validates_presence_of :name
 
-   def last_name
-   	 self.name.split.last
-   end
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
