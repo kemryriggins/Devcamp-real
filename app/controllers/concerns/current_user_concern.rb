f81ac6ca43eed4398308a8c7531e5fb3c1fcdb48
kemryriggins <98kariggins@gmail.com>
2017-12-13 10:18:47 -0700

    Updated edit.html

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,12 +1,12 @@
 module CurrentUserConcern
   extend ActiveSupport::Concern
 
-  def guest_user
-  	super || guest_user
+  def current_user
+    super || guest_user
   end
 
   def guest_user
-    guest = guest_user
+    guest = GuestUser.new
     guest.name = "Guest User"
     guest.first_name = "Guest"
     guest.last_name = "User"
