1236fd06bda6d19257ad2fa0468526a742cacba0
kemryriggins <98kariggins@gmail.com>
2017-12-21 15:36:25 -0700

    Implemented icons in breadcrumbs

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,25 +1,27 @@
 module BlogsHelper
-  def gravatar_helper user
-    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
-  end
+ def gravatar_helper user
+   image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)} (3 kB)
 
-  class CodeRayify < Redcarpet::Render::HTML
-    def block_code(code, language)
-      CodeRay.scan(code, language).div
-    end
-  end
+", width: 50
+ end
 
-  def markdown(text)
-    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+ class CodeRayify < Redcarpet::Render::HTML
+   def block_code(code, language)
+     CodeRay.scan(code, language).div
+   end
+ end
 
-    options = {
-      fenced_code_blocks: true,
-      no_intra_emphasis: true,
-      autolink: true,
-      lax_html_blocks: true,
-    }
+ def markdown(text)
+   coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
 
-    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-    markdown_to_html.render(text).html_safe
-  end
+   options = {
+     fenced_code_blocks: true,
+     no_intra_emphasis: true,
+     autolink: true,
+     lax_html_blocks: true,
+   }
+
+   markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+   markdown_to_html.render(text).html_safe
+ end
 end
