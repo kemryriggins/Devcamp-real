dc74e3f14c48ad6e3a1e8283f9549e1d2f777865
kemryriggins <98kariggins@gmail.com>
2017-12-22 15:10:26 -0700

    updated production file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require jquery3
+//= require popper
+//= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
 //= require cocoon
