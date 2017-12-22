e284ba94f7ecdc4dcd90bfc9d55243fe776ee62b
kemryriggins <98kariggins@gmail.com>
2017-12-22 14:53:20 -0700

    updated production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://kr-devcamp-portfolio.herokuapp.com', 'https://kr-devcamp-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://kr-devcamp-portfolio.herokuapp.com/cable"
 end
