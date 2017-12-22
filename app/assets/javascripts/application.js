020ac8747c7eaba846c2627fdc8e1385f17984eb
kemryriggins <98kariggins@gmail.com>
2017-12-22 13:21:14 -0700

    updated gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require popper
-//= require rails-ujs
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
 //= require jquery-ui
-//= require html.sortable
 //= require cocoon
 //= require gritter
 //= require cable
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-sprockets
